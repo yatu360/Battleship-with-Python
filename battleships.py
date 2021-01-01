4027b4dd2ec9f8437483cec066e0dd0c4c827f02
Yatu <yatu360gmail.comgit config --global user.email yatu360gmail.com>
2021-01-01 14:19:58 +0000

    Ship placement completed

--- battleships.py
+++ battleships.py
@@ -1,3 +1,5 @@
+import random
+
 def is_sunk(ship):
     #remove pass and add your implementation
     pass
@@ -7,20 +9,79 @@ def ship_type(ship):
     pass
 
 def is_open_sea(row, column, fleet):
-    #remove pass and add your implementation
-    pass
+    check = (row, column)
+    for x in range(len(fleet)):
+        if check in fleet[x][4]:
+            return False
+        else: 
+            for coord in fleet[x][4]:
+                r = coord[0]
+                c = coord[1]
+                if check == (r+1, c) or check ==(r, c+1) or check==(r-1, c) or check==(r, c-1) or check == (r-1, c-1) or check==(r+1, c+1) or check==(r+1, c-1) or check == (r-1, c+1):
+                    return False
+    #print(check)
+    return True
 
 def ok_to_place_ship_at(row, column, horizontal, length, fleet):
-    #remove pass and add your implementation
-    pass
+    row1 = 0
+    col1 = 0
+    count = 0
+    for x in range(length):
+        if horizontal == True:
+            if (column+length)>9:
+                return fleet
+            else:
+                col1 = column + x
+                if is_open_sea(row, col1, fleet) == True:
+                    count +=1
+                    if count == length:
+                        place_ship_at (row, column, horizontal, length, fleet)
+     
+        else:
+            if (row+length)>9:
+                return fleet
+            else:
+                row1 = row + x
+                if is_open_sea(row1, column, fleet) == True:
+                    count +=1
+                    if count == length:
+                        place_ship_at (row, column, horizontal, length, fleet)
 
 def place_ship_at(row, column, horizontal, length, fleet):
-    #remove pass and add your implementation
-    pass
+    ship = []
+    s = []
+    for x in range(length):
+        if horizontal == True:
+            ship_cod = (row, column+x)
+        else:
+            ship_cod = (row+x, column)
+        ship.append(ship_cod)
+    s.append(row)
+    s.append(column)
+    s.append(horizontal)
+    s.append(length)
+    s.append(set(ship))
+    fleet.append(tuple(s))    
+    return fleet
 
 def randomly_place_all_ships():
-    #remove pass and add your implementation
-    pass
+    fleet = []
+
+    while len(fleet) < 10:
+
+        row = random.randint(0, 9)
+        col = random.randint(0, 9)
+        horizontal = bool(random.getrandbits(1))
+        if len(fleet)<1:
+            length = 4
+        elif len(fleet)<3:
+            length = 3
+        elif len(fleet)<6:
+            length = 2
+        elif len(fleet) < 10:
+            length = 1
+        (ok_to_place_ship_at (row, col, horizontal, length, fleet))
+    return fleet
 
 def check_if_hits(row, column, fleet):
     #remove pass and add your implementation
